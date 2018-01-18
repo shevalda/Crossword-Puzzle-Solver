d42b4641248f3f972d0d0d3ac991f41a14386b52
Shevalda Gracielira <32480435+shevalda@users.noreply.github.com>
2018-01-19 05:19:29 +0700

    Updating fileToProgram function
    - Added when N is read
    - returns N, matrix, and word_list instead
      of passing it through arguement

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import copy
 
-def fileToProgram(file_name, matrix, word_list):
+def fileToProgram(file_name):
+    " Mengolah isi file eksternal "
     file = open(file_name, "r")
+    n = int(file.readline())
+    matrix = []
+    word_list = []
     for line in file:
         if (line[0] == "-") or (line[0] == "#"):    # matriks crossword
             temp = list(line.split())
@@ -10,12 +14,16 @@ def fileToProgram(file_name, matrix, word_list):
             word_list.extend(line.split(";"))
             print("word_list ", word_list)
     file.close()
+    return n, matrix, word_list
 
 def displayBoard(matrix):
+    " Menampilkan papan Crossword "
     for baris in matrix:
         for kolom in baris:
             print(kolom, end=" ")
         print()
 
-""" PROGRAM UTAMA """
+# PROGRAM UTAMA
 if __name__ == "__main__":
+    fn = input()    # file name
+    N, board, listOfWords = fileToProgram(fn)   # berapa N kotak, matriks papan, daftar kata
