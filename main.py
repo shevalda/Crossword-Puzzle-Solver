8e4010d8ec80c1f7084506f9969a9072765ace88
Shevalda Gracielira <32480435+shevalda@users.noreply.github.com>
2018-01-22 06:50:55 +0700

    Updating fileToProgram function and Adding checkingPlaceholder function
    fileToProgram function
    - the words in word_list is split for future convenience
    
    checkingPlaceholder function
    - save placeholders (blanks) in the board. saved as list of coordinates
    for future convenience

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import copy
-
 def fileToProgram(file_name):
     " Mengolah isi file eksternal "
     file = open(file_name, "r")
@@ -14,7 +12,43 @@ def fileToProgram(file_name):
         elif line != '\n':                          # daftar kata dalam crossword
             word_list.extend(line.split(";"))
     file.close()
+    
+    word_list = [[char for char in word] for word in word_list[:]]
+    
     return n, matrix, word_list
+
+def checkingPlaceholder(matrix,n):
+    li = []
+    hor_l = []
+    ver_l = []
+
+    for i in range(n):
+        for j in range(n):
+            # horizontal
+            if matrix[i][j] == '-':
+                hor_l.append((i,j))
+            elif len(hor_l) == 1:
+                hor_l = []
+            elif hor_l != []:
+                li.append(hor_l)
+                hor_l = []
+            
+            # vertical
+            if matrix[j][i] == '-':
+                ver_l.append((j,i))
+            elif len(ver_l) == 1:
+                ver_l = []
+            elif ver_l != []:
+                    li.append(ver_l)
+                    ver_l = []
+        if hor_l != []:
+            li.append(hor_l)
+            hor_l =[]
+        if ver_l != []:
+            li.append(ver_l)
+            ver_l = []
+
+    return li 
 
 def displayBoard(matrix):
     " Menampilkan papan Crossword "
@@ -29,4 +63,4 @@ if __name__ == "__main__":
     
     N, board, listOfWords = fileToProgram(fn)   # berapa N kotak, matriks papan, daftar kata
 
-    horizontal, vertical = searchForBlanks(board,N)
+    blankPlaceholder = checkingPlaceholder(board, N)
