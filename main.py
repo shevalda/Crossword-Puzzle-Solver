66a208a015633c7270d455dd38e93dd512069864
Shevalda Gracielira <32480435+shevalda@users.noreply.github.com>
2018-01-22 23:01:11 +0700

    Updating candidatesLengthThatIntersects function; Adding matchingWithCharOnBoard and isRemainingWordLengthUnique function
    candidatesLengthThatIntersects function
    - list returned only save the sum of words that can intersect chosen word
    
    matchingWithCharOnBoard function
    - eliminates word that cannot intersect words that were already written
    
    isRemainingWordLengthUnique function
    - checks whether the remaining words have unique length
    
    + checkIntersections function: "matrix" argument deleted
    + uniqueLength function: "placeholder" argument deleted
    + already works with sample3.txt and sample5.txt

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from copy import copy, deepcopy
+from copy import deepcopy
 
 def fileToProgram(file_name):
     " Mengolah isi file eksternal "
@@ -23,7 +23,6 @@ def checkingPlaceholder(matrix, n):
     temp_l = []
     
     # horizontal
-    li = []
     hor_l = []
     for i in range(n):
         for j in range(n):
@@ -55,7 +54,7 @@ def checkingPlaceholder(matrix, n):
 
     return hor_l, ver_l
 
-def checkIntersections(matrix, hor_l, ver_l):
+def checkIntersections(hor_l, ver_l):
     li = []
 
     for ph in hor_l:
@@ -66,7 +65,7 @@ def checkIntersections(matrix, hor_l, ver_l):
 
     return li
 
-def uniqueLength(placeholder, word_list):
+def uniqueLength(word_list):
     li = []
 
     for word in word_list:
@@ -86,11 +85,43 @@ def matchingUniquePlaceholder(placeholder, word):
         if len(ph) == len(word):
             return ph
 
-def candidatesThatIntersects(word, intersects, word_list):
+def candidatesLengthThatIntersects(word, intersects, word_list, placeholder):
     li = []
-    for intersect in intersects:
-        li.append([w for w in word_list if len(w) == len(word) and w[intersect[1].index(intersect[0])] == word[intersect[1].index(intersect[0])] and w != word])
+    word_sum = 0
+
+    for i in intersects:
+        for w in [wt for wt in word_list if len(wt) == len(i[1])]:
+            if w[i[1].index(i[0])] == word[placeholder.index(i[0])]:
+                word_sum += 1
+                # ## BEGIN - TO BE DELETED
+                # print("sum bcs", w)
+                # ## END - TO BE DELETED
+        li.extend([word_sum])
+        word_sum = 0
+
     return li
+
+def matchingWithCharOnBoard(matrix, placeholder, intersects, word_list):
+
+    for i in intersects:
+        if matrix[i[0][0]][i[0][1]] != '-':
+            for word in word_list[:]:
+                # ## BEGIN - TO BE DELETED
+                # print("index", i[1].index(i[0]))
+                # ## END - TO BE DELETED
+                if matrix[i[0][0]][i[0][1]] != word[placeholder.index(i[0])]:
+                    word_list.pop(word_list.index(word))
+    
+    return word_list
+
+def isRemainingWordLengthUnique(word_list):
+    if len(word_list) <= 1:
+        return True
+    else:
+        for word in word_list:
+            if len([w for w in word_list if w != word and len(w) == len(word)]) != 0:
+                return False
+        return True
 
 def displayBoard(matrix):
     " Menampilkan papan Crossword "
@@ -116,12 +147,13 @@ if __name__ == "__main__":
     usedPlaceholder = []
     notYetUsedPlaceholder = deepcopy(blankPlaceholder)
 
-    intersections = checkIntersections(board, holL, verL)
+    intersections = checkIntersections(holL, verL)
 
     # Mengisi kotak yang panjangnya unik
-    uniqueLengthWords = uniqueLength(notYetUsedPlaceholder, listOfWords)
+    uniqueLengthWords = uniqueLength(listOfWords)
 
     if uniqueLengthWords != [[]]:
+        # usedWords, notYetUsedPlaceholder, usedPlaceholder, notYetUsedWords, board = insertWordsWithUniqueLength(uniqueLengthWords, usedWords, notYetUsedWords, blankPlaceholder, usedPlaceholder, notYetUsedPlaceholder, board)
         for word in uniqueLengthWords:
             usedWords.append(word)
             notYetUsedWords.pop(notYetUsedWords.index(word))
@@ -137,36 +169,86 @@ if __name__ == "__main__":
         print()
     
     # while len(notYetUsedPlaceholder) > 0:
-    #     crntPlaceholder = notYetUsedPlaceholder.pop(0)
+    while not(isRemainingWordLengthUnique(notYetUsedWords)):
+        # ## BEGIN - TO BE DELETED
+        # if len(notYetUsedPlaceholder) == 2 or len(notYetUsedWords) == 2:
+        #     print("notYetUsedPlaceholder", notYetUsedPlaceholder)
+        #     print("notYetUsedWords", notYetUsedWords)
+        # ## END - TO BE DELETED
 
-    #     ## BEGIN - TO BE DELETED
-    #     print("crntPlaceholder", crntPlaceholder)
-    #     ## END - TO BE DELETED
+        crntPlaceholder = notYetUsedPlaceholder.pop(0)
 
-    #     inserted = False
-    #     word_candidates = [w for w in notYetUsedWords if len(w) == len(crntPlaceholder)]
-    #     intersects = [[p, ph] for p in crntPlaceholder for ph in blankPlaceholder if (ph != crntPlaceholder) and (p in ph)]
-    #     ## BEGIN - TO BE DELETED
-    #     print("intersects", intersects)
-    #     ## END - TO BE DELETED
-    #     for word in word_candidates:
-    #         ins_candidates = candidatesThatIntersects(word, intersects, notYetUsedWords)
+        # ## BEGIN - TO BE DELETED
+        # if len(notYetUsedPlaceholder) == 2 or len(notYetUsedWords) == 2:
+        #     print("crntPlaceholder", crntPlaceholder)
+        # ## END - TO BE DELETED
 
-    #         ## BEGIN - TO BE DELETED
-    #         print("word", word)
-    #         print("ins_candidates", ins_candidates)
-    #         ## END - TO BE DELETED
+        word_candidates = [w for w in notYetUsedWords if len(w) == len(crntPlaceholder)]
+        intersects = [[p, ph] for p in crntPlaceholder for ph in blankPlaceholder if (ph != crntPlaceholder) and (p in ph)]
+        
+        # ## BEGIN - TO BE DELETED
+        # if len(notYetUsedPlaceholder) == 2 or len(notYetUsedWords) == 2:
+        #     print("intersects", intersects)
+        #     print("word_candidates BEFORE", word_candidates)
+        # ## END - TO BE DELETED
 
-    #         # if # cek apakah aman utk memasukkan kata #:
-    #         #     inserted = True
-    #         #     board = insertWordOnBoard(board, crntPlaceholder, word)
-    #         #     usedWords.append(word)
-    #         #     notYetUsedWords.pop(notYetUsedWords.index(word))
-    #     # kalau masih tidak pasti
-    #     if not(inserted):
-    #         notYetUsedPlaceholder.append(crntPlaceholder)
+        # Jika ada interseksi yang sudah diisi, eliminasi kata yang tidak mungkin muat
+        word_candidates = matchingWithCharOnBoard(board, crntPlaceholder, intersects, word_candidates)
 
-    #     displayBoard(board)
-    
-    # print("After all words inserted")
-    # displayBoard(board)
+        # ## BEGIN - TO BE DELETED
+        # if len(notYetUsedPlaceholder) == 2 or len(notYetUsedWords) == 2:        
+        #     print("word_candidates AFTER ELIMINATION", word_candidates)
+        # ## END - TO BE DELETED
+
+        for word in word_candidates[:]:          
+            # ## BEGIN - TO BE DELETED
+            # if len(notYetUsedPlaceholder) == 2 or len(notYetUsedWords) == 2:
+            #     print("word", word)
+            # ## END - TO BE DELETED
+            
+            ins_candidates_len = candidatesLengthThatIntersects(word, intersects, listOfWords, crntPlaceholder)
+
+            # ## BEGIN - TO BE DELETED
+            # if len(notYetUsedPlaceholder) == 2 or len(notYetUsedWords) == 2:
+            #     print("ins_candidates_len", ins_candidates_len)
+            # ## END - TO BE DELETED
+
+            if not((0 not in ins_candidates_len) and (1 in ins_candidates_len)):
+                word_candidates.pop(word_candidates.index(word))
+            
+            # ## BEGIN - TO BE DELETED
+            # if len(notYetUsedPlaceholder) == 2 or len(notYetUsedWords) == 2:            
+            #     print("word_candidates AFTER", word_candidates)
+            # ## END - TO BE DELETED
+        
+        if len(word_candidates) == 1:
+            inserted = True
+            board = insertWordOnBoard(board, crntPlaceholder, word_candidates[0])
+            usedWords.append(word_candidates[0])
+            notYetUsedWords.pop(notYetUsedWords.index(word_candidates[0]))
+            
+            ## BEGIN - TO BE DELETED
+            displayBoard(board)
+            print()
+            ## END - TO BE DELETED
+        else:
+            notYetUsedPlaceholder.append(crntPlaceholder)
+        
+        # ## BEGIN - TO BE DELETED
+        # if len(notYetUsedPlaceholder) == 2 or len(notYetUsedWords) == 2:
+        #     break
+        # ## END - TO BE DELETED
+
+    if notYetUsedWords != []:
+        for word in notYetUsedWords[:]:
+            usedWords.append(word)
+            notYetUsedWords.pop(notYetUsedWords.index(word))
+
+            ph = matchingUniquePlaceholder(notYetUsedPlaceholder, word)
+            usedPlaceholder.append(ph)
+            notYetUsedPlaceholder.pop(notYetUsedPlaceholder.index(ph))
+
+            board = insertWordOnBoard(board, ph, word)  
+
+    print("After all words inserted")
+    displayBoard(board)
